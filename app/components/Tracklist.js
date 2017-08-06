81c425bc6eee1db77a15b0e171a636909eb046c1
Léo <leo.grivel@epitech.eu>
2017-08-07 00:31:33 +0200

    Modal added with different choices to play the song

--- app/components/Tracklist.js
+++ app/components/Tracklist.js
@@ -4,7 +4,9 @@ import {
   Text,
   StyleSheet,
   Image,
-  ScrollView
+  ScrollView,
+  TouchableHighlight,
+  Modal
 } from 'react-native';
 
 import Colors from '../styles/colors';
@@ -12,10 +14,19 @@ import Colors from '../styles/colors';
 class Tracklist extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showModal: false
+    };
   }
 
-  shouldComponentUpdate(nextProps) {
-    return this.props.tracks.length !== nextProps.tracks.length;
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.tracks.length !== nextProps.tracks.length || this.state.showModal !== nextState.showModal;
+  }
+
+  openModal(track) {
+    console.log('Track', track);
+    this.setState({showModal: true});
   }
 
   renderTracks() {
@@ -24,27 +35,31 @@ class Tracklist extends Component {
         {this.props.tracks.map(({track}, index) => {
           if (track !== undefined) {
             return (
-              <View
+              <TouchableHighlight
                 key={`Track${index}`}
                 style={styles.track}
+                onPress={this.openModal.bind(this, track)}
+                underlayColor={'transparent'}
               >
-                <Image
-                  style={styles.artwork}
-                  source={{uri: track.artwork_url || track.user.avatar_url}}
-                />
-                <View style={styles.rowContainer}>
-                  <View style={styles.firstRow}>
-                    <Text style={styles.h2}>{track.user.username}</Text>
-                    <Text style={styles.h2}>{track.duration}</Text>
-                  </View>
-                  <View>
-                    <Text>{track.title}</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.h2}>{track.playback_count}</Text>
+                <View style={styles.pressChild}>
+                  <Image
+                    style={styles.artwork}
+                    source={{uri: track.artwork_url || track.user.avatar_url}}
+                  />
+                  <View style={styles.rowContainer}>
+                    <View style={styles.firstRow}>
+                      <Text style={styles.h2}>{track.user.username}</Text>
+                      <Text style={styles.h2}>{track.duration}</Text>
+                    </View>
+                    <View>
+                      <Text>{track.title}</Text>
+                    </View>
+                    <View>
+                      <Text style={styles.h2}>{track.playback_count}</Text>
+                    </View>
                   </View>
                 </View>
-              </View>
+              </TouchableHighlight>
             );
           }
         })}
@@ -56,6 +71,29 @@ class Tracklist extends Component {
     return (
       <View style={styles.container}>
         {this.props.tracks.length > 0 && this.renderTracks()}
+        <Modal
+          animationType={'fade'}
+          visible={this.state.showModal}
+          onRequestClose={() => {}}
+          transparent={true}
+        >
+          <TouchableHighlight
+            style={styles.modal}
+            onPress={() => this.setState({showModal: false})}
+          >
+            <View style={styles.subModal}>
+              <View style={styles.modalTextWrapper}>
+                <Text style={styles.modalText}>{'Play it on the server'}</Text>
+              </View>
+              <View style={styles.modalTextWrapper}>
+                <Text style={styles.modalText}>{'Play it on the phone'}</Text>
+              </View>
+              <View style={styles.modalTextWrapper}>
+                <Text style={styles.modalText}>{'Download it'}</Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+        </Modal>
       </View>
     );
   }
@@ -85,6 +123,31 @@ const styles = StyleSheet.create({
   rowContainer: {
     flex: 1,
     justifyContent: 'space-between'
+  },
+  pressChild: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  subModal: {
+    backgroundColor: 'white',
+    width: 300,
+    height: 300,
+    borderRadius: 5,
+    elevation: 7
+  },
+  modal: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  modalTextWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalText: {
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 });
 
