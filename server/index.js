401e6c2a828c9db93650a00591362439919f2d76
Léo <leo.grivel@epitech.eu>
2017-08-07 20:30:51 +0200

    Express added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,6 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server is running`);
+app.listen(3000, () => {
+  console.log('Server is running');
 });
