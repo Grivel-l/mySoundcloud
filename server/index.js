ea05be8def44730bd3339106ae73b017f08ac1d5
Léo <leo.grivel@epitech.eu>
2017-08-07 21:07:30 +0200

    Routes loader done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routes = require('./routes/index.js');
+
 const app = express();
 
 app.listen(3000, () => {
   console.log('Server is running');
+  routes(app);
 });
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = app => {
+  const loadRoutes = (additionalPath = '') => {
+    const path = `${__dirname}/.${additionalPath}`
+    fs.readdirSync(path).map(file => {
+      if (file !== '.' && file !== '..') {
+        if (fs.statSync(`${path}/${file}`).isDirectory()) {
+          loadRoutes(`${additionalPath}/${file}`);
+        } else {
+          if (file.includes('.route.js')) {
+            require(`${path}/${file}`)(app);
+          }
+        }
+      }
+    });
+  };
+
+  loadRoutes();
+};
