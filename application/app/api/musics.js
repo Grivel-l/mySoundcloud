309ce5f3e74e503a356d216cc441462334741c96
Léo <leo.grivel@epitech.eu>
2017-08-16 10:42:10 +0200

    Added server_ip in the app config

--- application/app/api/musics.js
+++ application/app/api/musics.js
@@ -1,6 +1,6 @@
-import {CLIENT_ID,  USER_ID} from '../config/index';
+import {CLIENT_ID,  USER_ID, SERVER_IP} from '../config/index';
 import fetchAPI from './index';
 
 export const sendMusicAPI = ({idTrack}) => {
-  return fetchAPI(`http://192.168.0.20:3000/music/play?client_id=${CLIENT_ID}&idTrack=${idTrack}`);
+  return fetchAPI(`http://${SERVER_IP}/music/play?client_id=${CLIENT_ID}&idTrack=${idTrack}`);
 };
--- application/app/config/index.js
+++ application/app/config/index.js
@@ -1,2 +1,3 @@
 export const CLIENT_ID = '73cd8f8567fdd940d6c4ca9faa77e065';
 export const USER_ID = '97202795';
+export const SERVER_IP = '10.45.34.134:3000';
